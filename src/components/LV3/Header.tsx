1acc78d626f718fb6be8d8c0273711d5833d45bc
Tadahiko_Nihei <t.nihei@coc-labo.co.jp>
2024-05-06 23:04:34 +0900

    styled-components導入

--- src/components/LV3/Header.tsx
+++ src/components/LV3/Header.tsx
@@ -4,6 +4,30 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import HomeIcon from "@mui/icons-material/Home";
 import Link from "next/link";
+import styled from "styled-components";
+
+const IconWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  line-height: 46px;
+`;
+
+const LinkIcon = styled(Link)`
+  display: flex;
+  padding: 0;
+`;
+
+const HomeButton = styled(IconButton)`
+  display: flex;
+  padding: 0;
+`;
+
+const HeaderTitle = styled.div`
+  font-size: 16px;
+  margin-left: 8px;
+`;
 
 const Header: React.FC = () => {
   return (
@@ -13,11 +37,14 @@ const Header: React.FC = () => {
         style={{ color: "#ffffff", backgroundColor: "#5f5d5b" }}
       >
         <Toolbar>
-          <IconButton edge="start" color="inherit">
-            <Link href="/">
-              <HomeIcon />
-            </Link>
-          </IconButton>
+          <IconWrap>
+            <HomeButton color="inherit">
+              <LinkIcon href="/">
+                <HomeIcon />
+              </LinkIcon>
+            </HomeButton>
+            <HeaderTitle>dappe-practice</HeaderTitle>
+          </IconWrap>
         </Toolbar>
       </AppBar>
     </>
