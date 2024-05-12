451fa0774a6dcac98a0dbd597033f362560a235e
Tadahiko_Nihei <t.nihei@coc-labo.co.jp>
2024-05-12 14:51:31 +0900

    google-font追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>{"dappe-practice"}</title>
+        <meta property="description" content="練習用ページです。" />
+        <meta property="og:title" content="dappe-practice" />
+        <meta property="og:description" content="練習用ページです。" />
+        <link rel="icon" href="/favicon.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=VT323&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
