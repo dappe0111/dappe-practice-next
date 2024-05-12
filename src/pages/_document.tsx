4b47075c147dc2422ab3689954b3de6c02cd3f18
Tadahiko_Nihei <t.nihei@coc-labo.co.jp>
2024-05-12 16:20:23 +0900

    _document.tsx修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,58 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+type Props = {
+  styleTags: any;
+};
+
+export default class MyDocument extends Document<Props> {
+  static async getInitialProps(ctx: DocumentContext): Promise<
+    DocumentInitialProps & {
+      styles: JSX.Element;
+    }
+  > {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
