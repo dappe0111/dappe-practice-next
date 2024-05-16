677ac449a4d482bf16cdc32c210e4b4711e4c23c
Tadahiko_Nihei <t.nihei@coc-labo.co.jp>
2024-05-16 19:04:54 +0900

    TOP改修

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import CommonMeta from "@/components/LV1/CommonMeta";
 import Link from "next/link";
 
 const Main = styled.main`
-  height: 100vh;
   margin-top: 64px;
   display: flex;
   justify-content: flex-start;
@@ -15,8 +14,20 @@ const Main = styled.main`
   }
 `;
 
-const Block = styled.div`
+const BlockWrap = styled.main`
   width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: row;
+  @media screen and (max-width: 599px) {
+    flex-direction: column;
+  }
+`;
+
+const Block = styled.div<{ category?: string }>`
+  width: 50%;
   padding: 32px;
   display: flex;
   gap: 32px;
@@ -26,7 +37,9 @@ const Block = styled.div`
   @media screen and (max-width: 599px) {
     padding: 16px;
     gap: 16px;
+    width: 100%;
   }
+  ${({ category }) => getBlockStyle(category)}
 `;
 
 const BlockTitle = styled.div`
@@ -70,46 +83,79 @@ const ButtonText = styled.div`
   color: #ffffff;
 `;
 
+const getBlockStyle = (category?: string) => {
+  switch (category) {
+    case "top":
+      return `width: 100%`;
+    case "performance":
+      return `background-image: linear-gradient(to top,rgba(0, 0, 0, 0.7) 0%,
+          rgba(255, 255, 225, 0.7) 100%),url(/bg-block-1.jpg);background-size: cover;background-position: bottom;`;
+    case "game":
+      return `background-image: linear-gradient(to top,rgba(0, 0, 0, 0.7) 0%,
+          rgba(255, 255, 225, 0.7) 100%),url(/bg-block-2.jpg);background-size: cover;background-position: center;`;
+    default:
+      return ``;
+  }
+};
+
 export default function Home() {
   return (
     <>
       <CommonMeta title="TOP" />
       <Header />
       <Main>
-        <Block>
-          <BlockTitle>Performance</BlockTitle>
+        <Block category="top">
+          <BlockTitle>dappe-practice</BlockTitle>
           <Description>
-            AKAIのEWI SOLOを使って、様々なジャンルの曲を練習しています。
+            過去の実装経験を活かした記事や、趣味にまつわる話題まで
             <br />
-            EWI SOLOの多種多様なサウンドをお楽しみください。
+            ジャンルを問わず、格納していきます。
           </Description>
-          <ButtonWrap>
-            <Link href="/performance">
-              <Button>
-                <ButtonText>MORE</ButtonText>
-              </Button>
-            </Link>
-          </ButtonWrap>
         </Block>
-        <Block>
-          <BlockTitle>Game History</BlockTitle>
-          <Description>
-            今まで私が生きてきた中でプレイしてきたTVゲームを振り返る場です。
-            <br />
-            ネタバレがない範囲で、所感やコメントを残しています。
-          </Description>
-          <ButtonWrap>
-            <Link href="/game-history">
-              <Button>
-                <ButtonText>MORE</ButtonText>
-              </Button>
-            </Link>
-          </ButtonWrap>
-        </Block>
-        <Block>
-          <BlockTitle>Coming Soon...</BlockTitle>
-          <Description>公開までしばらくお待ちください。</Description>
-        </Block>
+        <BlockWrap>
+          <Block category="performance">
+            <BlockTitle>Performance</BlockTitle>
+            <Description>
+              AKAIのEWI SOLOを使って、様々なジャンルの曲を練習しています。
+              <br />
+              EWI SOLOの多種多様なサウンドをお楽しみください。
+            </Description>
+            <ButtonWrap>
+              <Link href="/performance">
+                <Button>
+                  <ButtonText>MORE</ButtonText>
+                </Button>
+              </Link>
+            </ButtonWrap>
+          </Block>
+          <Block category="game">
+            <BlockTitle>Game History</BlockTitle>
+            <Description>
+              今まで私が生きてきた中でプレイしてきたTVゲームを振り返る場です。
+              <br />
+              ネタバレがない範囲で、所感やコメントを残しています。
+            </Description>
+            <ButtonWrap>
+              <Link href="/game-history">
+                <Button>
+                  <ButtonText>MORE</ButtonText>
+                </Button>
+              </Link>
+            </ButtonWrap>
+          </Block>
+          <Block>
+            <BlockTitle>Technic</BlockTitle>
+            <Description>公開までしばらくお待ちください。</Description>
+          </Block>
+          <Block>
+            <BlockTitle>Hello! Project</BlockTitle>
+            <Description>公開までしばらくお待ちください。</Description>
+          </Block>
+          <Block category="top">
+            <BlockTitle>Coming Soon...</BlockTitle>
+            <Description>公開までしばらくお待ちください。</Description>
+          </Block>
+        </BlockWrap>
       </Main>
     </>
   );
