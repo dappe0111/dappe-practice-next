60bd3646cd73cc2278264cefda9452258fb46057
Tadahiko_Nihei <t.nihei@coc-labo.co.jp>
2024-05-16 19:40:18 +0900

    コンポーネント化

---
+++ src/components/LV3/PerformancePage.tsx
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+import Link from "next/link";
+import { YouTubeEmbed } from "@next/third-parties/google";
+
+const ContentsBlock = styled.div`
+  width: 100%;
+  padding: 32px;
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  @media screen and (max-width: 599px) {
+    padding: 16px;
+  }
+`;
+
+const ContentsTitle = styled.div`
+  font-size: 32px;
+`;
+
+const DescriptionWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+const ContentsDescription = styled.div`
+  font-size: 16px;
+  line-height: 1.5;
+  text-indent: -1em;
+  padding-left: 1em;
+`;
+
+const UpdateBlock = styled.div`
+  width: 100%;
+  padding: 32px;
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 599px) {
+    padding: 16px;
+  }
+`;
+
+const VideoWrap = styled.div`
+  width: 100%;
+  max-width: 640px;
+`;
+
+const ButtonWrap = styled.div`
+  width: 100%;
+  font-size: 16px;
+  padding: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled.div`
+  min-width: 160px;
+  padding: 8px 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #5f5d5b;
+  border-radius: 4px;
+  transition: 0.4s;
+  &:hover {
+    text-decoration: none;
+    opacity: 0.64;
+  }
+`;
+
+const ButtonText = styled.div`
+  font-size: 32px;
+  color: #ffffff;
+`;
+
+type Props = {
+  contents: any;
+};
+
+const PerformancePage: React.FC<Props> = ({ contents }) => {
+  return (
+    <>
+      <ContentsBlock>
+        <ContentsTitle>{contents.title}</ContentsTitle>
+        <DescriptionWrap>
+          <ContentsDescription>
+            ●作曲者：{contents.composer}
+          </ContentsDescription>
+          <ContentsDescription>●出典：{contents.source}</ContentsDescription>
+          <ContentsDescription>
+            <div dangerouslySetInnerHTML={{ __html: contents.comment }}></div>
+          </ContentsDescription>
+        </DescriptionWrap>
+      </ContentsBlock>
+      <UpdateBlock>
+        ※再生すると音声が流れます。音量にご注意ください。
+        <VideoWrap>
+          <YouTubeEmbed videoid={contents.videoId} />
+        </VideoWrap>
+      </UpdateBlock>
+      <ButtonWrap>
+        <Link href="/performance">
+          <Button>
+            <ButtonText>BACK</ButtonText>
+          </Button>
+        </Link>
+      </ButtonWrap>
+    </>
+  );
+};
+
+export default PerformancePage;
--- src/pages/performance/001.tsx
+++ src/pages/performance/001.tsx
@@ -1,8 +1,7 @@
 import Header from "@/components/LV3/Header";
 import styled from "styled-components";
 import CommonMeta from "@/components/LV1/CommonMeta";
-import Link from "next/link";
-import { YouTubeEmbed } from "@next/third-parties/google";
+import PerformancePage from "@/components/LV3/PerformancePage";
 
 const Main = styled.main`
   height: 100vh;
@@ -16,107 +15,25 @@ const Main = styled.main`
   }
 `;
 
-const ContentsBlock = styled.div`
-  width: 100%;
-  padding: 32px;
-  display: flex;
-  gap: 16px;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-`;
-
-const ContentsTitle = styled.div`
-  font-size: 32px;
-`;
-
-const ContentsDescription = styled.div`
-  font-size: 16px;
-  line-height: 1.5;
-  text-indent: -1em;
-  padding-left: 1em;
-`;
-
-const UpdateBlock = styled.div`
-  width: 100%;
-  padding: 32px;
-  display: flex;
-  gap: 16px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const VideoWrap = styled.div`
-  width: 100%;
-  max-width: 540px;
-`;
-
-const ButtonWrap = styled.div`
-  width: 100%;
-  font-size: 16px;
-  padding: 32px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Button = styled.div`
-  min-width: 160px;
-  padding: 8px 16px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #5f5d5b;
-  border-radius: 4px;
-  transition: 0.4s;
-  &:hover {
-    text-decoration: none;
-    opacity: 0.64;
-  }
-`;
-
-const ButtonText = styled.div`
-  font-size: 32px;
-  color: #ffffff;
-`;
-
 export default function Performance() {
+  const contents = {
+    title: "いつか帰るところ",
+    composer: "植松伸夫",
+    source: "ファイナルファンタジーIX",
+    comment: `●コメント：<br />ファイナルファンタジーIXのオープニングで流れるBGMです。
+            <br />
+            原曲はメインの旋律とサブの旋律の絶妙な掛け合いがあり、とても幻想的で素敵な楽曲です。
+            <br />
+            <br />
+            今回はオーボエの音色を使用し、メイン旋律のみを演奏しました。`,
+    videoId: "xW3CefYY4vA",
+  };
   return (
     <>
       <CommonMeta title="Performance" />
       <Header />
       <Main>
-        <ContentsBlock>
-          <ContentsTitle>いつか帰るところ</ContentsTitle>
-          <ContentsDescription>●作曲者：植松伸夫</ContentsDescription>
-          <ContentsDescription>
-            ●出典：ファイナルファンタジーIX
-          </ContentsDescription>
-          <ContentsDescription>
-            ●コメント：
-            <br />
-            ファイナルファンタジーIXのオープニングで流れるBGMです。
-            <br />
-            原曲はメインの旋律とサブの旋律の絶妙な掛け合いがあり、とても幻想的で素敵な楽曲です。
-            <br />
-            <br />
-            今回はオーボエの音色を使用し、メイン旋律のみを演奏しました。
-          </ContentsDescription>
-        </ContentsBlock>
-        <UpdateBlock>
-          ※再生すると音声が流れます。音量にご注意ください。
-          <VideoWrap>
-            <YouTubeEmbed videoid="xW3CefYY4vA" />
-          </VideoWrap>
-        </UpdateBlock>
-        <ButtonWrap>
-          <Link href="/performance">
-            <Button>
-              <ButtonText>BACK</ButtonText>
-            </Button>
-          </Link>
-        </ButtonWrap>
+        <PerformancePage contents={contents} />
       </Main>
     </>
   );
